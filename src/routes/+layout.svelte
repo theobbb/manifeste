d4e139fb5a787996532c5acfc0838a2e461f767e
Theo <theobbb64@gmail.com>
2026-02-05 06:12:19 -0500

    g

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,8 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<title>Manifeste</title>
+	<link rel="icon" href={favicon} />
+</svelte:head>
 {@render children()}
